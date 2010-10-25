4c3afa217046dde899fb512c4dbad45646dd582e
Christo Buschek <crito@cryptodrunks.net>
2010-10-25 03:09:54 +0200

    Added example code from http://www.danubepanorama.net/testing/player2/demo/.

--- src/hitchhiking.js
+++ src/hitchhiking.js
@@ -2,5 +2,6 @@
  * hitchhiking.js - All javascript relevant to the hitchhiking project. 
  *
  * see: http://mariazendre.org/hitchhiking/
+ * Use http://jsdoc.sourceforge.net/ to generate documentation
  */
 
